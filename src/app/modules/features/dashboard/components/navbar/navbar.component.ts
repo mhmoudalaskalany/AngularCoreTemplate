589cb9695a3b84f23ac536b23ba07b2e2d32e082
Ahmed Khairy <akhairy.c@channels.com.sa>
2020-07-02 22:09:42 +0200

    fix navbar UI

--- src/app/modules/features/dashboard/components/navbar/navbar.component.ts
+++ src/app/modules/features/dashboard/components/navbar/navbar.component.ts
@@ -9,22 +9,26 @@ import { TranslationService } from 'core/services/localization/translation.servi
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
-
+  lang;
   constructor(
     private dashboardService: DashboardService,
     private translateService: TranslationService
   ) { }
   isCollapsed = true;
   ngOnInit() {
+    this.translateService.currentLanguage.subscribe(lang => this.lang = lang);
   }
 
   toggleSidebarPin() {
     this.dashboardService.toggleSidebarPin();
   }
+
   toggleSidebar() {
     this.dashboardService.toggleSidebar();
   }
+
   setLanguage(lang: string): void {
+    this.lang = lang;
     this.translateService.setLanguage(lang);
     console.log('language', this.translateService.lang);
   }
